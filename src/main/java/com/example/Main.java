55637d7327b8c4e867d887f9c1eb1dc98f2d2ade
HaroldCH-UAM <124860853+HaroldCH-UAM@users.noreply.github.com>
2023-11-24 02:28:54 +0000

    Varios metodos agregados

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,14 +1,32 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Tree arbol = new Tree();
-        arbol.createExample();
-        arbol.inPreOrderViewTree();
-        System.out.println("///////////");
-        arbol.inOrderViewTree();
-        System.out.println("///////////");
-        arbol.inPostOrderViewTree();
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Tree arbol = new Tree();
+    arbol.createExample();
+
+    System.out.println(arbol.inPreOrderS());
+    System.out.println("///////////");
+
+    System.out.println(arbol.inOrderS());
+    System.out.println("///////////");
+
+    System.out.println(arbol.inPostOrderS());
+    System.out.println("///////////");
+    System.out.println(arbol.countNodes());
+    System.out.println("///////////");
+    System.out.println(arbol.grade());
+    System.out.println("///////////");
+    System.out.println(arbol.leaveCount());
+    System.out.println("///////////");
+    System.out.println(arbol.rootCount());
+    System.out.println("///////////");
+      System.out.println(arbol.isDegenerated());
+    Tree degenerado = new Tree();
+    degenerado.creaDegenerado();
+      System.out.println(degenerado.isDegenerated());
+    
+
+  }
 }
--- src/main/java/com/example/Tree.java
+++ src/main/java/com/example/Tree.java
@@ -1,60 +1,187 @@
 package com.example;
-
 public class Tree {
-    private Node root;
-    public Tree() {
-        root = null;
-    }
-    public void createExample(){
-        root = new Node('A');
+  private Node root;
 
-        root.left = new Node('B');
-        root.right = new Node('C');
+  public Tree() {
+    root = null;
+  }
 
-        root.left.left = new Node('D');
-        root.right.left = new Node('E');
-        root.right.right = new Node('F');
+  public void createExample() {
+    root = new Node('A');
 
-        root.left.left.right = new Node('G');
-        root.right.left.left = new Node('H');
-        root.right.left.right = new Node('I');
+    root.left = new Node('B');
+    root.right = new Node('C');
+
+    root.left.left = new Node('D');
+    root.right.left = new Node('E');
+    root.right.right = new Node('F');
+
+    root.left.left.right = new Node('G');
+    root.right.left.left = new Node('H');
+    root.right.left.right = new Node('I');
+  }
+
+  private void inPreOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    System.out.println(node.info);
+    inPreOrder(node.left);
+    inPreOrder(node.right);
+  }
+
+  public void inPreOrderViewTree() {
+    inPreOrder(root);
+  }
+
+  private void inOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    inOrder(node.left);
+    System.out.println(node.info);
+    inOrder(node.right);
+  }
+
+  public void inOrderViewTree() {
+    inOrder(root);
+  }
+
+  private void inPostOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+    inPostOrder(node.left);
+    inPostOrder(node.right);
+    System.out.println(node.info);
+  }
+
+  public void inPostOrderViewTree() {
+    inPostOrder(root);
+  }
+
+  private String inOrderS(Node node) {
+    if (node == null) {
+      return "";
+    }
+    String aux = "";
+    if (node.left != null) {
+      aux = inOrderS(node.left) + ", ";
+    }
+    aux += node.info;
+    if (node.right != null) {
+      aux += ", " + inOrderS(node.right);
+    }
+    return aux;
+  }
+
+  public String inOrderS() {
+    return inOrderS(root);
+  }
+
+  private String inPreOrderS(Node node) {
+    if (node == null) {
+      return "";
+    }
+    String aux = "" + node.info;
+    if (node.left != null) {
+      aux += ", " + inPreOrderS(node.left);
     }
 
-    private void inPreOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        System.out.println(node.info);
-        inPreOrder(node.left);
-        inPreOrder(node.right);
+    if (node.right != null) {
+      aux += ", " + inPreOrderS(node.right);
     }
+    return aux;
+  }
 
-    public void inPreOrderViewTree(){
-        inPreOrder(root);
+  public String inPreOrderS() {
+    return inPreOrderS(root);
+  }
+  private String inPostOrderS(Node node){
+    String aux = "";
+    if(node == null){return "";}
+    if(node.left != null){
+      aux = inPostOrderS(node.left);
     }
-    private void inOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        inOrder(node.left);
-        System.out.println(node.info);
-        inOrder(node.right);
+    if(node.right!= null){
+      if(node.left != null){
+        aux += ", " + inPostOrderS(node.right);
+      }else{
+        aux = inPostOrderS(node.right);
+      }
     }
+    if(node.left != null || node.right != null){
+      aux += ", " + node.info;
+    } else {
+      aux += ""+node.info;
+    }
+    return aux;
+  }
 
-    public void inOrderViewTree(){
-        inOrder(root);
+  public String inPostOrderS(){
+    return inPostOrderS(root);
+  }
+  private int countNodes(Node node){
+    if(node == null ) return 0;
+    return 1 + countNodes(node.left) + countNodes(node.right);
+  }
+  public int countNodes(){
+    return countNodes(root);
+  }
+  private int grade(Node node){
+    if(node == null) return 0;
+    return 1 + Math.max(grade(node.left), grade(node.right));
+  }
+  public int grade(){
+    return grade(root);
+  }
+  private String leaveCount(Node node){
+    if(node == null) return "";
+    String aux = "";
+    if(node.left == null && node.right == null){
+      aux += node.info + ", ";
     }
-    private void inPostOrder(Node node) {
-        if(node == null) {
-            return;
-        }
-        inPostOrder(node.left);
-        inPostOrder(node.right);
-        System.out.println(node.info);
-    }
+    aux += leaveCount(node.left) + leaveCount(node.right);
+    return aux;
+  }
+  public String leaveCount(){
+    return leaveCount(root);
+  }
 
-    public void inPostOrderViewTree(){
-        inPostOrder(root);
+  private String rootCount(Node node){
+    if(node == null) return "";
+    String aux = "";
+    if(node.left != null || node.right != null){
+      aux += node.info + ", ";
     }
+    aux += rootCount(node.left) + rootCount(node.right);
+    return aux;
+  }
+  public String rootCount(){
+    return rootCount(root);
+  }
+  private boolean isDegenerated(Node node){
+    if(node == null){return true;}
+  if(node.left != null && node.right != null){return false;}
+    return isDegenerated(node.left) && isDegenerated(node.right);
     
+  }
+  public boolean isDegenerated(){
+    return isDegenerated(root);
+  }
+  public void creaDegenerado( ){
+
+    root = new Node('A');
+
+    root.left = new Node('B');
+
+
+    root.left.left = new Node('D');
+
+
+    root.left.left.right = new Node('G');
+
+    root.left.left.right.left = new Node('X');
+
+  }
 }
